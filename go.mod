e77426760510714b02e451c504619341c361b466
Adam Primarizki <adam.primarizki@gmail.com>
2024-04-27 19:28:39 +0700

    initiate project

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/prmzk/go-base-prmzk
+
+go 1.22.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Go Gym API"))
+	})
+
+	log.Println("Go Gym Server is running on port 8080...")
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
